d1d4aded4a31e8f7dbeb95d2a5552baf9d88803b
Harsh Patel <harsh.patel@indellient.com>
2023-09-13 14:58:37 -0400

    Iac deployed via Pulumi to Aws
    This will deploy the Web Ui and Web API to aws via pulumi

---
+++ infra-team-test/infra-web/Startup.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Pulumi;
+using Pulumi.Aws.Ec2;
+using Pulumi.Aws.Ec2.Inputs;
+
+
+class Startup : Stack
+{
+    public class Start_up
+    {
+        public Start_up(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Start_up> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    using var hc = new HttpClient();
+                    using var apiResponse = await hc.GetAsync(Configuration["ApiAddress"]);
+                    var apiResult = await apiResponse.Content.ReadAsStringAsync();
+                    await context.Response.WriteAsync(apiResult);
+                });
+            });
+        }
+    }
+}
